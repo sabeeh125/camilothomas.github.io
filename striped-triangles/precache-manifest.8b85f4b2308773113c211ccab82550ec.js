e6161f92ab28935e9bd8b303afee01c41368ba13
camilothomas <cembre@gmail.com>
2019-04-30 01:40:42 -0500

    trying to add react build

---
+++ striped-triangles/precache-manifest.8b85f4b2308773113c211ccab82550ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76a3e668d82a50cd99e7883be51c8b5",
+    "url": "/striped-triangles/index.html"
+  },
+  {
+    "revision": "783b287af6f9830786e3",
+    "url": "/striped-triangles/static/css/main.0d640535.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/striped-triangles/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "783b287af6f9830786e3",
+    "url": "/striped-triangles/static/js/main.0f38030a.chunk.js"
+  },
+  {
+    "revision": "7f5cf7ed65ba03da85fa",
+    "url": "/striped-triangles/static/js/runtime~main.57351b2c.js"
+  }
+]);
