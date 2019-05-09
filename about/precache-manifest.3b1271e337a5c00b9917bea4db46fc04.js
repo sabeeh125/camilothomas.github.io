dccfe9c74367313a6f2d7aba7abbcca0790cacca
camilothomas <cembre@gmail.com>
2019-05-09 03:37:30 -0500

    completely changed the about page and moved it to own folder

---
+++ about/precache-manifest.3b1271e337a5c00b9917bea4db46fc04.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9e59fed056208c0f290a335c6abf04",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "833521372da9830d67bd",
+    "url": "/about/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "60b9bd2125254f65ca11",
+    "url": "/about/static/css/main.324d4d51.chunk.css"
+  },
+  {
+    "revision": "833521372da9830d67bd",
+    "url": "/about/static/js/2.a55ac973.chunk.js"
+  },
+  {
+    "revision": "60b9bd2125254f65ca11",
+    "url": "/about/static/js/main.aec2405a.chunk.js"
+  },
+  {
+    "revision": "5e7b65462749e51da51f",
+    "url": "/about/static/js/runtime~main.c0e869d3.js"
+  },
+  {
+    "revision": "2beea2528b5597fd9ac9a7aa90924503",
+    "url": "/about/static/media/Card_back_10.2beea252.svg"
+  },
+  {
+    "revision": "dd5c8815475115327d0614894e00a250",
+    "url": "/about/static/media/Card_back_12.dd5c8815.svg"
+  },
+  {
+    "revision": "4f9df8011ff64417f26890e8cc82591c",
+    "url": "/about/static/media/art.4f9df801.jpg"
+  },
+  {
+    "revision": "a88f05da20c9150198a3e96c5cc5f53c",
+    "url": "/about/static/media/music.a88f05da.jpg"
+  },
+  {
+    "revision": "71eb7bc4b26685b999574e2947d28fcf",
+    "url": "/about/static/media/travel.71eb7bc4.jpg"
+  }
+]);
